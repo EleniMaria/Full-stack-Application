8489f2cd295f987f109eee6e4eb207fa1fe70d62
Eleni-Maria <egoranites@hotmail.com>
2019-10-08 10:41:36 -0400

    updated routes on controller, setup connections for routes in hbs files.

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -1,78 +1,56 @@
 //organize by plants?
 const express = require('express');
 const router = express.Router();
-const GardenTip = require('../db/schema');
+const GardenTip = require('../db/schema');
 //new routes including delete
-//get data
+//get data index route
 router.get('/', (req, res) => {
-  GardenTip.find({}).then(GardenTips => {
-    res.render("index",GardenTips);
+  GardenTip.find({}).then(gardenTips => {
+    res.render('index', { gardenTips });
   });
 });
-//Find by ID
+//Find by ID show item
 router.get('/:id', (req, res) => {
-  GardenTip.findById(req.params.id).then(GardenTip => {
-    res.json(GardenTip);
+  GardenTip.findById(req.params.id).then(gardenTips => {
+    res.render('show', { gardenTips });
   });
 });
 //create new
-router.post('/', (req, res) => {
-  GardenTip.insert(req.body).then(GardenTip => {
-    GardenTip.find({}).then(GardenTips => {
-      res.json(GardenTips);
+router.get('/new', (req, res) => {
+  GardenTip.insert(req.body).then(gardenTips => {
+    GardenTip.find({}).then(gardenTips => {
+      res.render('new', { gardenTips });
     });
   });
 });
-//edit/update
+//post handler to create a new item
+router.post('/', (req, res) => {
+  GardenTip.create(req.body).then(gardenTips => {
+    res.redirect('/');
+  });
+});
+//edit
+router.get('/edit/:id', (req, res) => {
+  GardenTip.findOne({ _id: req.params.id }).then(gardenTips => {
+    res.render('edit', { gardenTips });
+  });
+});
+// //update
 router.put('/:id', (req, res) => {
-  GardenTip.findOneAndUpdate({ _id: req.params.id }, req.body).then(GardenTip =>
-    GardenTip.find({}).then(GardenTips => {
-      res.json(GardenTips);
-    })
-  );
+  GardenTip.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  }).then(gardenTips => {
+    res.redirect('/', { gardenTips });
+  });
 });
 //delete one item
+
 router.delete('/:id', (req, res) => {
-  GardenTip.findOneAndRemove({ _id: req.params.id }, req.body).then(GardenTip =>
-    GardenTip.find({}).then(GardenTips => {
-      res.json(GardenTips);
+  GardenTip.findOneAndRemove({ _id: req.params.id })
+    .then(() => {
+      res.redirect('/')
     })
-  );
-});
+})
 
 //Keep Last
 module.exports = router;
-
-//creating routes
-// router.get('/', (req, res) => {
-
-// //   res.send('<h1>Hello Fellow Gardener</h1>');
-// //   // res.json({ hello: 'fellow gardener' });
-// });
-// //import Model1
-// const Model1 = require('../db/models/Model1');
-// router.get('/', (req, res) => {
-//   // use this to find all Model1 data
-//   Model1.find({}).then(model1 => res.json(model1));
-// });
-// //Model1 Schema route for title
-// router.get('/:title', (req, res) => {
-//   console.log('it works');
-//   res.send('here is the title: ' + req.params.title);
-//   console.log(req.params);
-//   //find by title
-//   Model1.find({ title: req.params.title }).then(model1 => res.json(model1));
-//   console.log(req.params.title);
-// });
-// //route listens for create requests
-// router.post('/', (req, res) => {
-//   let newModel1 = req.body;
-//   console.log(newModel1);
-//   res.json(newModel1);
-// });
-// //route listens for update
-// router.put('/', (req, res) => {
-//   let newModel1 = req.body;
-//   console.log(newModel1);
-//   res.json(newModel1);
-// });
--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 //starts express 
 const express = require('express');
 const app = express();
+//starts methodOverride(keep above controller)
 const methodOverride = require("method-override");
-//setup parser 
+//starts parser (keep above controller)
 const parser = require('body-parser');
+//setup handlebars
 app.set('view engine','hbs');
+//setup parser(keep above controller)
 app.use(parser.urlencoded({ extended : true }));
 app.use(parser.json());
-//method Override
+//method Override setup(keep above controller)
 app.use(methodOverride("_method"));
-//setup handlebars
-// const hbs = require('express-handlebars');
-app.set('view engine','hbs');
-// app.engine('.hbs', hbs({
-//     extname: '.hbs',
-//     partialsDir: 'views/',
-//     layoutsDir: 'views/',
-//     defaultLayout: 'layout-main'
-//   }));
 //setup cors
 const cors = require('cors');
 app.use(cors());
-// app.get('/products/:id', function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-
-// app.listen(80, function () {
-//   console.log('CORS-enabled web server listening on port 80')
-// })
 //controllers setup
 const controller1Controller = require('./controllers/controller1');
-// app.use('api/controller1/', controller1Controller);
-// //second controller
-// const controller2Controller = require('./controllers/controller2');
-// app.use('api/controller2/', controller2Controller);
-//Local host
 app.use(controller1Controller);
+//Local host
 app.listen(8080, () => console.log('We are on Port 8080'));
