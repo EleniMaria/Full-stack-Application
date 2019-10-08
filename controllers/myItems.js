a11af6a041725257503ba519de48ac10ed0bfcb5
Eleni-Maria <egoranites@hotmail.com>
2019-10-08 14:43:33 -0400

    renamed controller, modified hbs files

--- controllers/myItems.js
+++ controllers/myItems.js
@@ -1,50 +1,51 @@
-//organize by plants?
+//router setup
 const express = require('express');
 const router = express.Router();
+//model schema
 const GardenTip = require('../db/schema');
 //new routes including delete
+
 //get data index route
 router.get('/', (req, res) => {
-  GardenTip.find({}).then(gardenTips => {
-    res.render('index', { gardenTips });
+  GardenTip.find({}).then(GardenTips => {
+    res.render('index', { GardenTips });
   });
 });
 //Find by ID show item
 router.get('/:id', (req, res) => {
-  GardenTip.findById(req.params.id).then(gardenTips => {
-    res.render('show', { gardenTips });
+  GardenTip.findById(req.params.id).then(GardenTips => {
+    res.render('show', { GardenTips });
   });
 });
 //create new
 router.get('/new', (req, res) => {
-  GardenTip.insert(req.body).then(gardenTips => {
-    GardenTip.find({}).then(gardenTips => {
-      res.render('new', { gardenTips });
+  GardenTip.insert(req.body).then(GardenTips => {
+    GardenTip.find({}).then(GardenTips => {
+      res.render('new', { GardenTips });
     });
   });
 });
 //post handler to create a new item
-router.post('/', (req, res) => {
-  GardenTip.create(req.body).then(gardenTips => {
-    res.redirect('/');
-  });
-});
+// router.post('/', (req, res) => {
+//   GardenTip.create(req.body).then(GardenTips => {
+//     res.redirect('/');
+//   });
+// });
 //edit
 router.get('/edit/:id', (req, res) => {
-  GardenTip.findOne({ _id: req.params.id }).then(gardenTips => {
-    res.render('edit', { gardenTips });
+  GardenTip.findOne({ _id: req.params.id }).then(GardenTips => {
+    res.render('edit', { GardenTips });
   });
 });
 // //update
 router.put('/:id', (req, res) => {
   GardenTip.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true
-  }).then(gardenTips => {
-    res.redirect('/', { gardenTips });
+  }).then(GardenTips => {
+    res.redirect('/', { GardenTips });
   });
 });
 //delete one item
-
 router.delete('/:id', (req, res) => {
   GardenTip.findOneAndRemove({ _id: req.params.id })
     .then(() => {
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-//starts express 
+//starts express (top)
 const express = require('express');
 const app = express();
 //starts methodOverride(keep above controller)
 const methodOverride = require("method-override");
 //starts parser (keep above controller)
 const parser = require('body-parser');
-//setup handlebars
+//setup handlebars(keep above controller)
+const hbs = require('hbs');
 app.set('view engine','hbs');
 //setup parser(keep above controller)
 app.use(parser.urlencoded({ extended : true }));
 app.use(parser.json());
 //method Override setup(keep above controller)
 app.use(methodOverride("_method"));
-//setup cors
+//setup cors(keep above controller)
 const cors = require('cors');
 app.use(cors());
-//controllers setup
-const controller1Controller = require('./controllers/controller1');
-app.use(controller1Controller);
-//Local host
+//controllers setup(keep last)
+const myItemsController = require('./controllers/myItems');
+app.use("/GardenTips",myItemsController);
+//Local host(bottom)
 app.listen(8080, () => console.log('We are on Port 8080'));
